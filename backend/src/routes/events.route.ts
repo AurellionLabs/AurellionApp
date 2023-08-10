f52c166e3fa4f7b179514d705cad5a8aad1db048
Rouge Shadow <matthewpinnock.mp@gmail.com>
2023-08-10 19:24:22 +0100

    fixed naming of endpoints to conform best practices

--- backend/src/routes/events.route.ts
+++ backend/src/routes/events.route.ts
@@ -4,12 +4,12 @@ const router = express.Router();
 import { Location } from '../common/types.js';
 import { getAllEventsForType, killOldEvents } from '../controllers/events.controller';
 
-router.get('/getAllEventsForType',(req, res, next) => {
+router.get('/get-all-events-for-type',(req, res, next) => {
     res.locals.id = req.query.id
     res.locals.type = req.query.type
     next()
 } ,getAllEventsForType)
 
-router.post('/KillOldEvents',killOldEvents)
+router.post('/kill-old-events',killOldEvents)
 
 export default router

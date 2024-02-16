870a6934136d20144ee7598cb304ac71ee104461
RougeShadow <44998993+RougeShadow@users.noreply.github.com>
2024-02-16 22:01:39 +0000

    Revert "Feat/jobs screen changes"

--- aurellion/src/screens/jobs/components/jobItem.tsx
+++ aurellion/src/screens/jobs/components/jobItem.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { SelectedBox, TextRow, StyledSelectedBox } from './StyledComponents';
+import { SelectedBox } from './StyledComponents';
 import { Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SignatureScreenNavigationProp } from '../../../navigation/types';
 import { useMainContext } from '../../main.provider';
-import { Journey, ParcelData } from '../../../common/types/types';
-import { StyledText } from '../../../common/components/StyledComponents';
+import { Journey } from '../../../common/types/types';
 
 type BoxProps = {
   job: Journey;
@@ -14,7 +13,6 @@ type BoxProps = {
 const JobItem: React.FC<BoxProps> = ({ job }) => {
   const { userType } = useMainContext();
   const navigation = useNavigation<SignatureScreenNavigationProp>();
-  const { setDeliveryOption, isDarkMode } = useMainContext();
   const onPress = () => {
     if (userType === 'customer') {
       navigation.navigate('Signature', {
@@ -28,48 +26,9 @@ const JobItem: React.FC<BoxProps> = ({ job }) => {
     }
   };
   return (
-    <StyledSelectedBox isDarkMode={isDarkMode} onPress={onPress}>
-      <TextRow>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '20%', fontWeight: 'bold', marginRight: 8 }}>
-          Job ID:
-        </StyledText>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '80%' }} numberOfLines={1} ellipsizeMode="tail">
-          {job.jobId}
-        </StyledText>
-      </TextRow>
-      <TextRow>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '20%', fontWeight: 'bold', marginRight: 8 }}>
-          Status:
-        </StyledText>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '80%' }} numberOfLines={1} ellipsizeMode="tail">
-          {job.currentStatus == '0'
-            ? 'Pending'
-            : job.currentStatus == '1'
-            ? 'InProgress'
-            : job.currentStatus == '2'
-            ? 'Completed'
-            : job.currentStatus == '3'
-            ? 'Canceled'
-            : 'Unknown Status'}
-        </StyledText>
-      </TextRow>
-      <TextRow>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '20%', fontWeight: 'bold', marginRight: 8 }}>
-          Start:
-        </StyledText>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '80%' }} numberOfLines={1} ellipsizeMode="tail">
-          {job.parcelData.startName}
-        </StyledText>
-      </TextRow>
-      <TextRow>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '20%', fontWeight: 'bold', marginRight: 8 }}>
-          End:
-        </StyledText>
-        <StyledText isDarkMode={isDarkMode} style={{ width: '80%' }} numberOfLines={1} ellipsizeMode="tail">
-          {job.parcelData.endName}
-        </StyledText>
-      </TextRow>
-    </StyledSelectedBox>
+    <SelectedBox onPress={onPress}>
+      <Text>{job.jobId}</Text>
+    </SelectedBox>
   );
 };
 

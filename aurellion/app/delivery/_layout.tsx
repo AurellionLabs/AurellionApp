e7a1b2b61531bc4adc3758c7cf546984f1a32c9e
Dineth Perera <dineth_perera@hotmail.com>
2024-07-13 01:08:26 +0100

    fixed bug with missing return statement in _layout files

--- aurellion/app/delivery/_layout.tsx
+++ aurellion/app/delivery/_layout.tsx
@@ -2,11 +2,13 @@ import DeliveryProvider from "@/providers/delivery.provider";
 import { Stack } from "expo-router";
 
 export default function DeliveryLayout() {
-  <DeliveryProvider>
-    <Stack>
-      <Stack.Screen name="customer" options={{ headerShown: false }} />
-      <Stack.Screen name="driver" options={{ headerShown: false }} />
-      <Stack.Screen name="signature" options={{ headerShown: false }} />
-    </Stack>
-  </DeliveryProvider>;
+  return (
+    <DeliveryProvider>
+      <Stack>
+        <Stack.Screen name="customer" options={{ headerShown: false }} />
+        <Stack.Screen name="driver" options={{ headerShown: false }} />
+        <Stack.Screen name="signature" options={{ headerShown: false }} />
+      </Stack>
+    </DeliveryProvider>
+  );
 }
--- aurellion/app/delivery/driver/_layout.tsx
+++ aurellion/app/delivery/driver/_layout.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "expo-router";
 
-export default function DriverLayout(){
+export default function DriverLayout() {
+  return (
     <Stack>
-        <Stack.Screen name="assignDriver" options={{headerShown: false}}/>
+      <Stack.Screen name="assignDriver" options={{ headerShown: false }} />
     </Stack>
+  );
 }

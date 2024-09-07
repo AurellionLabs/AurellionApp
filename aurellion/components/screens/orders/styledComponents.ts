a4ccc1cb33bca4d9f3320c56fe3946fb25b6e003
GaveenA <s3766914@student.rmit.edu.au>
2024-09-07 21:00:07 +1000

    new nodeItems component with support for asset image and darkMode

--- aurellion/components/screens/orders/styledComponents.ts
+++ aurellion/components/screens/orders/styledComponents.ts
@@ -6,10 +6,31 @@ interface ThemedProps {
   isDarkMode: boolean;
 }
 
+interface CustomProps {
+  [key: string]: any;
+}
+
+interface Props {
+  height: number;
+  boxState: boolean;
+  customProps: CustomProps;
+  boxSelected: boolean;
+}
+
+interface props {
+  isDarkMode: boolean;
+}
+
 export const OrderItem = styled.TouchableOpacity<ThemedProps>`
   padding: 12px;
   margin: 4px;
   border-radius: 12px;
   background-color: ${({ isDarkMode }) =>
-    isDarkMode ? DarkTheme.background1 : LightTheme.background1};
+    isDarkMode ? DarkTheme.background2 : LightTheme.background1};
+`;
+
+export const TextRow = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-bottom: 8px;
 `;

780b960e329e4d774b550e4fe73ebc7902237cce
RougeShadow <matthewpinnock.mp@gmail.com>
2024-09-24 01:02:34 +0100

    up to date contracts compile

--- blockchain/contracts/AuSys.sol
+++ blockchain/contracts/AuSys.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.17;
 import "./Aura.sol" as AuraContract;
+import "./Aurum.sol";
+import "./AuraGoat.sol";
 
 // TO DO use smart contracct account pf the token for the treasury and create a private fuinctioion that auto mints
 contract locationContract {
@@ -20,16 +22,30 @@ contract locationContract {
         string startName;
         string endName;
 
-        //add customer?
+        //add sender?
         //add driver?
         //add box
     }
-
+    struct Order {
+        bytes32 id;
+        address token;
+        uint[] tokenId;
+        uint[] tokenQuantity;
+        uint price;
+        address amount;
+        uint txFee;
+        address customer;
+        bytes32[] journeys;
+        address[] nodes;
+        ParcelData locationData;
+        Status currentStatus;
+        string contracatualAgreement;
+    }
     struct Journey {
         ParcelData parcelData;
-        bytes32 jobId;
+        bytes32 journeyId;
         Status currentStatus;
-        address customer;
+        address sender;
         address reciever;
         address driver;
         uint journeyStart;
@@ -43,84 +59,84 @@ contract locationContract {
     }
 
     // Map a journey to multiple sub journeys
-    mapping(bytes32 => mapping(uint256 => SubJourney)) journeys;
 
     // Keep count of which sub journey of a journey the parcel is on
-    mapping(bytes32 => uint256) subJourneyCount;
-
+    mapping(bytes32 => Order) public idToOrder;
+    mapping(bytes32 => bytes32) public journeyToOrderId;
+    mapping(bytes32 => uint256) public subJourneyCount;
     // Map the drivers address to a Journey/SubJourney, need to add address => uint => bytes32
     // need to map this to a list of bytes
-    mapping(address => bytes32[]) public driverToJobId;
-    // maps a customer to a job
-    mapping(address => bytes32[]) public customerToJobId;
-    mapping(address => uint256) public numberOfJobsCreatedForCustomer;
+    mapping(address => bytes32[]) public driverToJourneyId;
+    // maps a sender to a journey
+    mapping(address => bytes32[]) public customerToJourneyId;
+    mapping(address => uint256) public numberOfJourneysCreatedForCustomer;
     // driver related mappings
-    mapping(address => uint256) public numberOfJobsAssigned;
+    mapping(address => uint256) public numberOfJourneysAssigned;
+    // maps a receiver to a journey
+    mapping(address => bytes32[]) public receiverToJourneyId;
+    mapping(address => uint256) public numberOfJourneysCreatedForReceiver;
+    // Map Journey ID to Journey
+    mapping(bytes32 => Journey) public journeyIdToJourney;
 
-    // maps a receiver to a job
-    mapping(address => bytes32[]) public receiverToJobId;
-    mapping(address => uint256) public numberOfJobsCreatedForReceiver;
-    // Map Job ID to Journey
-    mapping(bytes32 => Journey) public jobIdToJourney;
-
-    // maps number to JOB id for the purpose of iterating through jobs
-    mapping(uint => bytes32) public numberToJobID;
-    // a bool that checks if the customer has handed off the package (need to change this to address => journey or job id => bool
+    // maps number to JOB id for the purpose of iterating through journeys
+    mapping(uint => bytes32) public numberToJourneyID;
+    // a bool that checks if the sender has handed off the package (need to change this to address => journey or journey id => bool
     mapping(address => mapping(bytes32 => bool)) public customerHandOff;
     mapping(address => mapping(bytes32 => bool)) public driverHandOn;
-    //maps a job to a corresponding box
-    mapping(bytes32 => uint) jobToBox;
-    // maps a customer address to running balance of their token amount
+    //maps a journey to a corresponding box
+    mapping(bytes32 => uint) journeyToBox;
+    // maps a sender address to running balance of their token amount
     mapping(address => uint) customerToTokenAmount;
-
     Journey[] public subJourneys;
-    uint public jobIdCounter = 0;
+    uint public journeyIdCounter = 0;
     AuraContract.Aura auraToken;
+    AurumNodeManager nodeManager;
 
-    constructor(AuraContract.Aura _aura) {
+    constructor(AuraContract.Aura _aura, AurumNodeManager _nodeManager) {
         auraToken = _aura;
+        nodeManager = _nodeManager;
     }
 
     // vulnerability somebody
     modifier customerDriverCheck(
-        address customer,
+        address sender,
         address driver,
         bytes32 id
     ) {
         require(
-            (jobIdToJourney[id].driver == driver &&
-                jobIdToJourney[id].customer == customer) ||
-                jobIdToJourney[id].reciever == customer,
+            (journeyIdToJourney[id].driver == driver &&
+                journeyIdToJourney[id].sender == sender) ||
+                journeyIdToJourney[id].reciever == sender,
             "Was not correct 1"
         );
         require(
-            msg.sender == customer || msg.sender == driver,
+            msg.sender == sender || msg.sender == driver,
             "Was not correct 2"
         );
         _;
     }
     modifier DriversBoxVerify(address driver, uint box) {
-        //require(jobToBox[driverToJobId[driver]] == box);
+        //require(journeyToBox[driverToJourneyId[driver]] == box);
         _;
     }
     modifier isInProgress(bytes32 id) {
         require(
-            jobIdToJourney[id].currentStatus == Status.InProgress,
-            "Job is not in Progress"
+            journeyIdToJourney[id].currentStatus == Status.InProgress,
+            "Journey is not in Progress"
         );
         _;
     }
     modifier isPending(bytes32 id) {
         require(
-            jobIdToJourney[id].currentStatus == Status.Pending,
-            "Job is not Pending"
+            journeyIdToJourney[id].currentStatus == Status.Pending,
+            "Journey is not Pending"
         );
         _;
     }
     modifier isCompleted(bytes32 id) {
         require(
-            jobIdToJourney[id].currentStatus == Status.Completed,
-            "Job is Incomplete"
+            journeyIdToJourney[id].currentStatus == Status.Completed,
+            "Journey is Incomplete"
         );
         _;
     }
@@ -131,28 +147,32 @@ contract locationContract {
         return keccak256(abi.encode(journey));
     }
 
-    function getHashedJobId() private returns (bytes32) {
-        return keccak256(abi.encode(jobIdCounter += 1));
+    function getHashedJourneyId() private returns (bytes32) {
+        return keccak256(abi.encode(journeyIdCounter += 1));
     }
 
     event emitSig(address indexed user, bytes32 indexed id);
 
-    //could you exploit this feature by an agent calling from a non aurellion source  assign themseleves to all jobs then not showing up
-    function assignDriverToJobId(address driver, bytes32 jobID) public {
-        driverToJobId[driver].push(jobID);
-        jobIdToJourney[jobID].driver = driver;
-        numberOfJobsAssigned[driver] += 1;
+    function getjourney(bytes32 id) public view returns (Journey memory){
+        return journeyIdToJourney[id];
+
+    }
+        //could you exploit this feature by an agent calling from a non aurellion source  assign themseleves to all journeys then not showing up
+    function assignDriverToJourneyId(address driver, bytes32 journeyID) public {
+        driverToJourneyId[driver].push(journeyID);
+        journeyIdToJourney[journeyID].driver = driver;
+        numberOfJourneysAssigned[driver] += 1;
     }
 
-    //customer can be both reciever and customer
+    //sender can be both reciever and sender
     function packageSign(
         address driver,
-        address customer,
+        address sender,
         bytes32 id
-    ) public customerDriverCheck(customer, driver, id) {
-        if (msg.sender == customer) {
-            customerHandOff[customer][id] = true;
-            emit emitSig(customer, id);
+    ) public customerDriverCheck(sender, driver, id) {
+        if (msg.sender == sender) {
+            customerHandOff[sender][id] = true;
+            emit emitSig(sender, id);
         }
 
         if (msg.sender == driver) {
@@ -161,7 +181,7 @@ contract locationContract {
         }
 
         if (
-            customerHandOff[customer][id] == true &&
+            customerHandOff[sender][id] == true &&
             driverHandOn[driver][id] == true
         ) {
             emit emitSig(driver, id);
@@ -179,42 +199,48 @@ contract locationContract {
     // verify person has enough funds in contract
     // TODO: function wasn't working because the isCompleted modifier was wrong. Corrected it, need to retest.
     function generateReward(bytes32 id, address driver) public isCompleted(id) {
-        uint completeJourney = jobIdToJourney[id].journeyEnd -
-            jobIdToJourney[id].journeyStart;
+        uint completeJourney = journeyIdToJourney[id].journeyEnd -
+            journeyIdToJourney[id].journeyStart;
         emit printUint(completeJourney);
         // to find reward you didvide the ETA by the time it was completed in. this will give you a number less than 1 which you then multiply the reward by to give the driver  a fraction of that reward.
         // if driver completed it quicker it will do the opposite .
-        //need to consider whether the treausry gives extra reward for the faster delivery proportional to the excess of the fraction( the amount the customer didnt provide but is entitled to the driver as bonus.
+        //need to consider whether the treausry gives extra reward for the faster delivery proportional to the excess of the fraction( the amount the sender didnt provide but is entitled to the driver as bonus.
         //may have to * by x**10*y to make sure decimals are taken into account
-        uint reward = (jobIdToJourney[id].ETA *
-            jobIdToJourney[id].bounty *
+        uint reward = (journeyIdToJourney[id].ETA *
+            journeyIdToJourney[id].bounty *
             10 ** 18) / completeJourney;
         emit printUint(reward);
         // transfer reward here
         auraToken.transfer(driver, reward);
-        customerToTokenAmount[jobIdToJourney[id].customer] -= jobIdToJourney[id]
-            .bounty;
+        customerToTokenAmount[
+            journeyIdToJourney[id].sender
+        ] -= journeyIdToJourney[id].bounty;
     }
 
     event printUint(uint256 value);
 
-    function assignJobToBox(bytes32 job, uint box) public {
-        jobToBox[job] = box;
+    function assignJourneyToBox(bytes32 journey, uint box) public {
+        journeyToBox[journey] = box;
     }
 
     function handOn(
         address driver,
-        address customer,
+        address sender,
         bytes32 id
-    ) public customerDriverCheck(customer, driver, id) isPending(id) returns (bool) {
+    )
+        public
+        customerDriverCheck(sender, driver, id)
+        isPending(id)
+        returns (bool)
+    {
         if (
-            customerHandOff[customer][id] == true &&
-            driverHandOn[driver][id] == true 
+            customerHandOff[sender][id] == true &&
+            driverHandOn[driver][id] == true
         ) {
-            jobIdToJourney[id].currentStatus = Status.InProgress;
-            jobIdToJourney[id].journeyStart = block.timestamp;
+            journeyIdToJourney[id].currentStatus = Status.InProgress;
+            journeyIdToJourney[id].journeyStart = block.timestamp;
             driverHandOn[driver][id] == false;
-            customerHandOff[customer][id] == false;
+            customerHandOff[sender][id] == false;
             return true;
         } else {
             return false;
@@ -224,7 +250,9 @@ contract locationContract {
     function handOff(
         address driver,
         address reciever,
-        bytes32 id
+        bytes32 id,
+        //pass 0x0 if addr not requred
+        address token
     )
         public
         isInProgress(id)
@@ -235,10 +263,57 @@ contract locationContract {
             customerHandOff[reciever][id] == true &&
             driverHandOn[driver][id] == true
         ) {
-            jobIdToJourney[id].currentStatus = Status.Completed;
-            jobIdToJourney[id].journeyEnd = block.timestamp;
-
+            journeyIdToJourney[id].currentStatus = Status.Completed;
+            journeyIdToJourney[id].journeyEnd = block.timestamp;
             generateReward(id, driver);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // when specifying please specify quantity for a given tokenID at the same
+    // in the token qauntity list
+
+    function nodeHandOff(
+        address sendingNode,
+        address driver,
+        address reciever,
+        bytes32 id,
+        uint256[] memory tokenIds,
+        address token,
+        uint[] memory quantities,
+        bytes memory data
+    ) public returns (bool) {
+        //APPROVE BEFORE CALLING
+        //perform the transfer per token
+        Order memory order = idToOrder[journeyToOrderId[id]];
+        if (
+            bytes1(nodeManager.getNode(reciever).validNode) == bytes1(uint8(1)) ||
+            reciever == order.customer
+        ) {
+            handOff(driver, reciever, id, token);
+            (bool success, bytes memory result) = token.call(
+                abi.encodeWithSignature(
+                    "safeBatchTransferFrom(adress,address,uint256,uin256,bytes)",
+                    journeyIdToJourney[id].sender,
+                    journeyIdToJourney[id].reciever,
+                    tokenIds,
+                    quantities,
+                    data
+                )
+            );
+            require(success);
+            //reducing the capacity of the recieving nod if it is a
+            if (reciever == order.customer) {
+                order.currentStatus = Status.Completed;
+                nodeManager.updateSupportedAssets(tokenIds,quantities,sendingNode);
+                uint nodeReward = order.txFee / order.nodes.length;
+
+                for (uint i = 0; i > order.nodes.length; i++)
+                    auraToken.transfer(order.nodes[i],nodeReward);
+            }
             return true;
         } else {
             return false;
@@ -248,9 +323,9 @@ contract locationContract {
     //TO DO make function to send funds to treasury
 
     //function uploads(ParcelData memory _data) public {
-    //Journey memory journey = Journey({parcelData: _data, jobId: getHashedJobId(), currentStatus: Status.Pending, customer: address(0), driver: address(0), reciever: address(0) });
-    //jobIdToJourney[journey.jobId] = journey;
-    //jobToBox[journey.jobId] = jobIdCounter;
+    //Journey memory journey = Journey({parcelData: _data, journeyId: getHashedJourneyId(), currentStatus: Status.Pending, sender: address(0), driver: address(0), reciever: address(0) });
+    //journeyIdToJourney[journey.journeyId] = journey;
+    //journeyToBox[journey.journeyId] = journeyIdCounter;
 
     // SubJourney memory subJourney = SubJourney({parcelData: _array[i]});
 
@@ -261,21 +336,21 @@ contract locationContract {
     // journeys[journeyKeyHashing(journey)][subJourneyCount[journeyKeyHashing(journey)]] = subJourney;
     // SubJourney memory xyz = journeys[journeyKeyHashing(journey)][subJourneyCount(journeyKeyHashing(journey))+=1];
     // }
-    function jobCreation(
-        address customer,
+    function journeyCreation(
+        address sender,
         address reciever,
         ParcelData memory _data,
         uint bounty,
         uint ETA
     ) public {
-        // TO DO transfer bounty  from customer to contract make mapping of customer => tokens and make a withdraw function later
-        auraToken.transferFrom(customer, address(this), bounty * 10 ** 18);
-        customerToTokenAmount[customer] += bounty;
+        // TO DO transfer bounty  from sender to contract make mapping of sender => tokens and make a withdraw function later
+        auraToken.transferFrom(sender, address(this), bounty * 10 ** 18);
+        customerToTokenAmount[sender] += bounty;
         Journey memory journey = Journey({
             parcelData: _data,
-            jobId: getHashedJobId(),
+            journeyId: getHashedJourneyId(),
             currentStatus: Status.Pending,
-            customer: customer,
+            sender: sender,
             driver: address(0),
             reciever: reciever,
             journeyStart: 0,
@@ -283,15 +358,61 @@ contract locationContract {
             bounty: bounty,
             ETA: ETA
         });
-        jobIdToJourney[journey.jobId] = journey;
-        jobToBox[journey.jobId] = jobIdCounter;
+        journeyIdToJourney[journey.journeyId] = journey;
+        journeyToBox[journey.journeyId] = journeyIdCounter;
 
-        numberOfJobsCreatedForCustomer[customer] += 1;
-        customerToJobId[customer].push(journey.jobId);
+        numberOfJourneysCreatedForCustomer[sender] += 1;
+        customerToJourneyId[sender].push(journey.journeyId);
 
-        numberOfJobsCreatedForReceiver[reciever] += 1;
-        receiverToJobId[reciever].push(journey.jobId);
-        // add jobId to global mapping of jobs
-        numberToJobID[jobIdCounter] = journey.jobId;
+        numberOfJourneysCreatedForReceiver[reciever] += 1;
+        receiverToJourneyId[reciever].push(journey.journeyId);
+        // add journeyId to global mapping of journeys
+        numberToJourneyID[journeyIdCounter] = journey.journeyId;
+    }
+
+    function orderJourneyCreation(
+        bytes32 orderId,
+        address sender,
+        address reciever,
+        ParcelData memory _data,
+        uint bounty,
+        uint ETA
+    ) public {
+        // TO DO transfer bounty  from sender to contract make mapping of sender => tokens and make a withdraw function later
+        auraToken.transferFrom(sender, address(this), bounty * 10 ** 18);
+        customerToTokenAmount[sender] += bounty;
+        Journey memory journey = Journey({
+            parcelData: _data,
+            journeyId: getHashedJourneyId(),
+            currentStatus: Status.Pending,
+            sender: sender,
+            driver: address(0),
+            reciever: reciever,
+            journeyStart: 0,
+            journeyEnd: 0,
+            bounty: bounty,
+            ETA: ETA
+        });
+        journeyIdToJourney[journey.journeyId] = journey;
+        journeyToBox[journey.journeyId] = journeyIdCounter;
+
+        numberOfJourneysCreatedForCustomer[sender] += 1;
+        customerToJourneyId[sender].push(journey.journeyId);
+
+        numberOfJourneysCreatedForReceiver[reciever] += 1;
+        receiverToJourneyId[reciever].push(journey.journeyId);
+        // add journeyId to global mapping of journeys
+        numberToJourneyID[journeyIdCounter] = journey.journeyId;
+        idToOrder[orderId].journeys.push(journey.journeyId);
+        idToOrder[orderId].currentStatus = Status.Pending;
+        journeyToOrderId[journey.journeyId] = orderId;
+    }
+
+    function orderCreation(Order memory order) public {
+        bytes32 id = getHashedJourneyId();
+        idToOrder[id] = order;
+        idToOrder[id].currentStatus = Status.Pending;
+        idToOrder[id].id = id;
+        idToOrder[id].txFee = (order.price * 2) / 100;
     }
 }
---
+++ blockchain/contracts/Aurum.sol
@@ -0,0 +1,268 @@
+// SPDX-License-Identifier: MIT
+import "./Aura.sol";
+import "./AuSys.sol";
+import "./AuraGoat.sol";
+pragma solidity 0.8.26;
+
+contract AurumNodeManager {
+    struct Location {
+        string lat;
+        string lng;
+    }
+    struct LocationData {
+        Location startLocation;
+        Location endLocation;
+        string startName;
+        string endName;
+
+        //add customer?
+        //add driver?
+        //add box
+    }
+    struct Node {
+        string location;
+        //stteal ausys location struct
+        bytes1 validNode;
+        address owner;
+        uint256[] supportedAssets;
+        bytes1 status;
+        uint256[] capacity;
+        //capacity needs to be kept on an asset by asset basis
+    }
+    struct Item {
+        string category;
+        string subCategory;
+        string Type;
+    }
+    uint256 public nodeIdCounter = 0;
+    mapping(address => address[]) ownedNodes;
+    mapping(address => Node) AllNodes;
+    struct test {
+        string location;
+        //stteal ausys location struct
+        bytes1 validNode;
+        address owner;
+        uint256[] supportedAssets;
+        bytes1 status;
+        uint256[] capacity;
+    }
+    mapping(address => test) tests;
+    locationContract ausys;
+    AuraGoat auraGoat;
+    address admin;
+
+    constructor(locationContract _ausys, AuraGoat _auraGoat, address _admin) {
+        ausys = _ausys;
+        auraGoat = _auraGoat;
+        admin = _admin;
+    }
+
+    modifier adminOnly() {
+        require(msg.sender == admin);
+        _;
+    }
+    modifier isOwner(address node) {
+        address owner = AllNodes[node].owner;
+        require(msg.sender == owner);
+        _;
+    }
+
+    function setAdmin(address _admin) public adminOnly {
+        admin = _admin;
+    }
+
+    function registerNode(Node memory node) public returns (address id) {
+        aurumNode NodeContract = new aurumNode(node.owner, ausys, auraGoat,this);
+        id = address(NodeContract);
+        AllNodes[id] = node;
+        AllNodes[id].validNode = bytes1(uint8(1));
+        updateOwner(node.owner, id);
+    }
+
+    function getNode(address nodeAddress) public returns (Node memory node) {
+        node = AllNodes[nodeAddress];
+    }
+
+    //require both parties to sign on item addition
+    //create a node package sign function that mints to the node from aura when
+    // thhe node succesfully calls hand off
+    event eventUpdateOwner(address owner, address node);
+
+    function updateOwner(address owner, address node) public isOwner(node) {
+        AllNodes[node].owner = owner;
+        ownedNodes[owner].push(node);
+        emit eventUpdateOwner(owner, node);
+    }
+
+    event eventUpdateLocation(string location, address node);
+
+    function updateLocation(string memory location, address node) public {
+        AllNodes[node].location = location;
+        emit eventUpdateLocation(location, node);
+    }
+
+    event eventUpdateStatus(bytes1 status, address node);
+
+    //secure dis
+    function gasSafeUpdateCapacity(
+        address node,
+        uint256[] memory quantities,
+        uint256[] memory assets
+    ) public isOwner(node) {
+        Node storage targetNode = AllNodes[node];
+        for (uint256 i = 0; i > AllNodes[node].supportedAssets.length; i++) {
+            if (targetNode.supportedAssets[i] == assets[i]) {
+                targetNode.capacity[i] = quantities[i];
+                break;
+            }
+        }
+    }
+
+    function expensiveFuzzyUpdateCapacity(
+        address node,
+        uint256[] memory quantities,
+        uint256[] memory assets
+    ) public isOwner(node) {
+        Node storage targetNode = AllNodes[node];
+        require(assets.length == targetNode.supportedAssets.length);
+        for (uint256 k = 0; k > assets.length; k++)
+            for (
+                uint256 i = 0;
+                i > AllNodes[node].supportedAssets.length;
+                i++
+            ) {
+                if (targetNode.supportedAssets[i] == assets[k]) {
+                    targetNode.capacity[i] = quantities[i];
+                    break;
+                }
+            }
+    }
+
+    function updateStatus(bytes1 status, address node) public isOwner(node) {
+        AllNodes[node].status = status;
+        emit eventUpdateStatus(status, node);
+    }
+
+    event eventUpdateSupportedAssets(string[] supportedAssets, uint[] capacity);
+
+    function updateSupportedAssets(
+        uint[] memory supportedAssetsList,
+        uint[] memory capacity,
+        address nodeAddress
+    ) public {
+        Node storage node = AllNodes[nodeAddress];
+        delete node.supportedAssets;
+        delete node.capacity;
+
+        // Copy each element from memory array to storage array
+        for (uint256 i = 0; i < supportedAssetsList.length; i++) {
+            node.supportedAssets.push(supportedAssetsList[i]);
+            node.capacity.push(capacity[i]);
+        }
+    }
+
+    function nodeHandoff(
+        address node,
+        address driver,
+        address reciever,
+        bytes32 id,
+        uint256[] memory tokenIds,
+        address token,
+        uint256[] memory quantities,
+        bytes memory data
+    ) public  {
+        address sender  = ausys.getjourney(id).sender;
+        if (msg.sender == sender && AllNodes[node].validNode == bytes1(uint8(1)) && AllNodes[node].owner == msg.sender)
+            ausys.nodeHandOff(
+                msg.sender,
+                driver,
+                reciever,
+                id,
+                tokenIds,
+                token,
+                quantities,
+                data
+            );
+        //implement node tax (so take a cut from the protocol fee and)
+
+        // job creator has to pay this (if job creator is a node he only has to pay the burnt part)
+    }
+    //WHENEVER A NODE TRIGGERS HAND OFF IT GETS PAID OUT (WHAT ABOUT LOOPS WHERE PEOPLE SEND THE SAME PKG BACK AND FORTH)
+    //capacity
+    //handshake
+    // add new item (tokenise)
+    //mint token with added item so onchain rep
+    // erc1155 with unique id for goat grae and multiple stuff in  ID
+    //delete item
+}
+
+contract aurumNode {
+    address public owner;
+    locationContract ausys;
+    AuraGoat auraGoat;
+    AurumNodeManager manager;
+    constructor(address _owner, locationContract _ausys, AuraGoat _auraGoat, AurumNodeManager _manager) {
+        owner = _owner;
+        ausys = _ausys;
+        auraGoat = _auraGoat;
+        manager = _manager;
+    }
+
+    function nodeHandoff(
+        address node,
+        address driver,
+        address reciever,
+        bytes32 id,
+        uint256[] memory tokenIds,
+        address token,
+        uint256[] memory quantities,
+        bytes memory data
+    ) public  {
+        address sender  = ausys.getjourney(id).sender;
+        if (msg.sender == sender && manager.getNode(node).validNode == bytes1(uint8(1)) && manager.getNode(node).owner == msg.sender)
+            ausys.nodeHandOff(
+                msg.sender,
+                driver,
+                reciever,
+                id,
+                tokenIds,
+                token,
+                quantities,
+                data
+            );
+        //implement node tax (so take a cut from the protocol fee and)
+
+        // job creator has to pay this (if job creator is a node he only has to pay the burnt part)
+    }
+
+    function nodeHandOn(address driver, address reciever, bytes32 id) public {
+        ausys.handOn(driver, reciever, id);
+    }
+
+    function nodeSign(address node, address driver, bytes32 jobid) public {
+        ausys.packageSign(driver, node, jobid);
+    }
+
+    function addItem(
+        address itemOwner,
+        bytes32 id,
+        uint256 weight,
+        uint256 amount,
+        address item,
+        bytes memory data
+    ) public {
+        auraGoat.nodeMint(itemOwner, weight, amount, data);
+        //add the item to the node
+        // data should be a abi.encode of the entire data struct of an asset
+        // data should be decoded in the mint function of the desired asset
+        //unpack data
+        // mint goat tokens to node and have them transferred wherver the package goes
+        //(bool success, bytes memory result) = item.call(
+        //    abi.encodeWithSignature("mint(adress,bytes)", itemOwner, data)
+        //);
+        //     AllNodes[id].capacity -= quantity;
+        //require(success);
+        //(bool success, bytes memory result) = addr.call(abi.encodeWithSignature("myFunction(uint,address)", 10, msg.sender));
+    }
+}
+

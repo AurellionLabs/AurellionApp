b15a2252b4ded539a6edfc03254259920d80af34
GaveenA <s3766914@student.rmit.edu.au>
2024-10-15 20:04:39 +0530

    adding Autocomplete location screen to Node Registration Screen, retrofitting Virtualized List (autocomplete field) into Node Reg Screen, updating styling of location field in locationMenu

--- aurellion/app/(root)/node/registerNode.tsx
+++ aurellion/app/(root)/node/registerNode.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, Text, Image } from "react-native";
+import { FlatList, Text, Image, View } from "react-native";
 import {
   Container,
   Input,
@@ -7,7 +7,7 @@ import {
   Section,
 } from "@/components/screens/settings/styledComponents";
 import { useMainContext } from "@/providers/main.provider";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import {
   Heading,
   ImageContainer,
@@ -17,6 +17,10 @@ import {
 import { DarkTheme, LightTheme } from "@/constants/Colors";
 import DropDownPicker from "react-native-dropdown-picker";
 import { router } from "expo-router";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GMAPS_API_KEY, textInputStyles, GeoLocationCoords, Geometry } from '@/components/screens/createDelivery/locationsMenu';
+import * as Location from 'expo-location';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function RegisterNode() {
   const { isDarkMode } = useMainContext();
@@ -25,6 +29,18 @@ export default function RegisterNode() {
   const [walletAddress, setWalletAddress] = useState("");
   const [capacity, setCapacity] = useState("");
   const [assets, setAssets] = useState(null);
+
+  const [currentAddress, setCurrentAddress] = useState<string>("");
+  const [currentLocationCoords, setCurrentLocationCoords] = useState<Geometry>({
+    location: { lat: 0, lng: 0 },
+  });
+  const currentLocationGeo = {
+    description: currentAddress,
+    formatted_address: currentAddress,
+    geometry: currentLocationCoords, 
+  };
+  const nodeLocationAutocompleteRef: any = useRef(null);
+  const [nodeAddress, setNodeAddress] = useState("Enter Node address");
 
   const [assetsOpen, setAssetsOpen] = useState(false);
 
@@ -46,80 +62,170 @@ export default function RegisterNode() {
     router.replace("/node/addAsset");
   };
 
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.log('Permission to access location was denied');
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+        setCurrentLocationCoords({
+          location: { lat: latitude, lng: longitude },
+        });
+        
+        fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GMAPS_API_KEY}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            const address = data.results[0].formatted_address;
+            setCurrentAddress(address);
+          })
+          .catch((error) => console.error(error));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    requestLocationPermission();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View>
+      {item}
+    </View>
+  );
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <Container isDarkMode={isDarkMode}>
-          <Heading isDarkMode={isDarkMode}>Register Node</Heading>
-          <ImageContainer style={{ marginTop: 10 }}>
-            <Image
-              source={require("@/assets/images/user.png")}
-              style={{ height: 100, width: 100 }}
-            />
-          </ImageContainer>
-          <Section>
-            <Label isDarkMode={isDarkMode}>Node Name</Label>
-            <Input
-              value={nodeName}
-              onChangeText={setNodeName}
-              placeholder="Enter Node Name"
-              isDarkMode={isDarkMode}
-              placeholderTextColor={
-                isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1
-              }
-            />
-          </Section>
-          <Section>
-            <Label isDarkMode={isDarkMode}>Wallet Address</Label>
-            <Input
-              value={walletAddress}
-              onChangeText={setWalletAddress}
-              placeholder="Enter Wallet Address"
-              isDarkMode={isDarkMode}
-              placeholderTextColor={
-                isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1
-              }
-            />
-          </Section>
-          <Section>
-            <Label isDarkMode={isDarkMode}>Supported Assets</Label>
-            <DropDownPicker
-              open={assetsOpen}
-              value={assets}
-              items={assetItems}
-              setOpen={setAssetsOpen}
-              setValue={setAssets}
-              setItems={setAssetItems}
-              theme={isDarkMode ? "DARK" : "LIGHT"}
-              multiple={true}
-              min={1}
-              mode="BADGE"
-              placeholder="Select Assets"
-              listMode="SCROLLVIEW"
-              searchable={true}
-              zIndex={2}
-            />
-          </Section>
-          <Section>
-            <Label isDarkMode={isDarkMode}>Capacity</Label>
-            <Input
-              value={capacity}
-              onChangeText={setCapacity}
-              placeholder="Enter Node Capacity"
-              isDarkMode={isDarkMode}
-              placeholderTextColor={
-                isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1
-              }
-            />
-          </Section>
-          <RedButton
-            onPress={registerNode}
-            style={{ alignSelf: "center", marginTop: "7%" }}
-          >
-            <RedButtonText>Register Node</RedButtonText>
-          </RedButton>
-        </Container>
-      </ScrollView>
+      <FlatList
+        data={[
+          <Container isDarkMode={isDarkMode}>
+            <Heading isDarkMode={isDarkMode}>Register Node</Heading>
+            <ImageContainer style={{ marginTop: 10 }}>
+              <Image
+                source={require("@/assets/images/user.png")}
+                style={{ height: 100, width: 100 }}
+              />
+            </ImageContainer>
+            <Section>
+              <Label isDarkMode={isDarkMode}>Node Name</Label>
+              <Input
+                value={nodeName}
+                onChangeText={setNodeName}
+                placeholder="Enter Node Name"
+                isDarkMode={isDarkMode}
+                placeholderTextColor={
+                  isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1
+                }
+              />
+            </Section>
+            <Section>
+              <Label isDarkMode={isDarkMode}>Wallet Address</Label>
+              <Input
+                value={walletAddress}
+                onChangeText={setWalletAddress}
+                placeholder="Enter Wallet Address"
+                isDarkMode={isDarkMode}
+                placeholderTextColor={
+                  isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1
+                }
+              />
+            </Section>
+            <Section>
+              <View>
+                <Label isDarkMode={isDarkMode}>Location</Label>
+                <GooglePlacesAutocomplete
+                  listViewDisplayed="auto"
+                  ref={nodeLocationAutocompleteRef}
+                  placeholder={nodeAddress}
+                  onPress={(data, details = null) => {
+                    let address = details?.formatted_address || '';
+                    setNodeAddress(address);
+                    console.log(address);
+                  }}
+                  fetchDetails={true}
+                  query={{
+                    key: GMAPS_API_KEY,
+                    language: 'en',
+                  }}
+                  styles={textInputStyles(isDarkMode)}
+                  textInputProps={{
+                    onFocus: () => {
+                      if(nodeAddress == currentLocationGeo.formatted_address) {
+                        setNodeAddress('Enter Node address');
+                        nodeLocationAutocompleteRef.current?.setAddressText('');
+                        nodeLocationAutocompleteRef.placeholder = 'Enter Node address';
+                      }
+                    },
+                    clearButtonMode: 'never',
+                    placeholderTextColor: isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1,
+                  }}
+                  renderRightButton={() => (
+                    <Ionicons name="close" size={20} color={
+                      isDarkMode ? DarkTheme.foreground1 : LightTheme.foreground1
+                    } onPress={() => {
+                      setNodeAddress('Enter Node address');
+                      nodeLocationAutocompleteRef.current?.setAddressText('');
+                      nodeLocationAutocompleteRef.placeholder = 'Enter Node address';
+                  }} 
+                      style={{
+                        display: nodeAddress !== 'Enter Node address' ? 'flex' : 'none',
+                        position: 'absolute',
+                        right: 0,
+                        top: 10,
+                        zIndex: 1,
+                      }}/>)}
+                    predefinedPlaces={[currentLocationGeo, { description: 'Current Location', formatted_address: currentLocationGeo.formatted_address, geometry: currentLocationCoords }]}
+                  />
+              </View>
+            </Section>
+            <Section>
+              <Label isDarkMode={isDarkMode}>Supported Assets</Label>
+              <DropDownPicker
+                open={assetsOpen}
+                value={assets}
+                items={assetItems}
+                setOpen={setAssetsOpen}
+                setValue={setAssets}
+                setItems={setAssetItems}
+                theme={isDarkMode ? "DARK" : "LIGHT"}
+                multiple={true}
+                min={1}
+                mode="BADGE"
+                placeholder="Select Assets"
+                listMode="SCROLLVIEW"
+                searchable={true}
+                zIndex={2}
+              />
+            </Section>
+            <Section>
+              <Label isDarkMode={isDarkMode}>Capacity</Label>
+              <Input
+                value={capacity}
+                onChangeText={setCapacity}
+                placeholder="Enter Node Capacity"
+                isDarkMode={isDarkMode}
+                placeholderTextColor={
+                  isDarkMode ? DarkTheme.foreground2 : LightTheme.foreground1
+                }
+              />
+            </Section>
+            <RedButton
+              onPress={registerNode}
+              style={{ alignSelf: "center", marginTop: "7%" }}
+            >
+              <RedButtonText>Register Node</RedButtonText>
+            </RedButton>
+          </Container>
+        ]}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        keyboardShouldPersistTaps="always"
+      />
     </SafeAreaView>
   );
 }

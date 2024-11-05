8e5dd7a41e56ac43ca951813c9659f0456bdc834
Dineth Perera <dineth_perera@hotmail.com>
2024-11-05 22:11:34 +0000

    added fetching driver available and assigned journeys

--- aurellion/app/(tabs)/driver/yourJourneys.tsx
+++ aurellion/app/(tabs)/driver/yourJourneys.tsx
@@ -6,49 +6,54 @@ import { useDriverContext } from "@/providers/driver.provider";
 import { useEffect } from "react";
 import { JourneyStatus } from "@/constants/Types";
 import { Heading } from "@/components/common/StyledComponents";
+import { fetchDriverAssignedJourneys } from "@/dapp-connectors/dapp-controller";
 
 export default function YourJourneys() {
   const { isDarkMode } = useMainContext();
   const { setYourJourneys, yourJourneys } = useDriverContext();
 
   useEffect(() => {
-    // TODO: load and set available orders from chain
-    setYourJourneys([
-      {
-        parcelData: {
-          startLocation: { lat: "52.397312", lng: "-1.505571" },
-          endLocation: { lat: "52.404645", lng: "-1.520389" },
-          startName: "Start Location",
-          endName: "End Location",
-        },
-        bounty: 10,
-        currentStatus: JourneyStatus.PENDING,
-        customer: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B45",
-        driver: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B56",
-        ETA: 100,
-        jobId: "123",
-        journeyStart: 12,
-        journeyEnd: 13,
-        reciever: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0gr4",
-      },
-      {
-        parcelData: {
-          startLocation: { lat: "52.397312", lng: "-1.505571" },
-          endLocation: { lat: "52.404645", lng: "-1.520389" },
-          startName: "Start Location",
-          endName: "End Location",
-        },
-        bounty: 10,
-        currentStatus: JourneyStatus.PENDING,
-        customer: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B45",
-        driver: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B56",
-        ETA: 100,
-        jobId: "123",
-        journeyStart: 12,
-        journeyEnd: 13,
-        reciever: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0gr4",
-      },
-    ]);
+    const getAssignedJourneys = async () => {
+      const journeys = await fetchDriverAssignedJourneys();
+      setYourJourneys(journeys);
+    };
+    getAssignedJourneys();
+    // setYourJourneys([
+    //   {
+    //     parcelData: {
+    //       startLocation: { lat: "52.397312", lng: "-1.505571" },
+    //       endLocation: { lat: "52.404645", lng: "-1.520389" },
+    //       startName: "Start Location",
+    //       endName: "End Location",
+    //     },
+    //     bounty: 10,
+    //     currentStatus: JourneyStatus.PENDING,
+    //     customer: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B45",
+    //     driver: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B56",
+    //     ETA: 100,
+    //     jobId: "123",
+    //     journeyStart: 12,
+    //     journeyEnd: 13,
+    //     reciever: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0gr4",
+    //   },
+    //   {
+    //     parcelData: {
+    //       startLocation: { lat: "52.397312", lng: "-1.505571" },
+    //       endLocation: { lat: "52.404645", lng: "-1.520389" },
+    //       startName: "Start Location",
+    //       endName: "End Location",
+    //     },
+    //     bounty: 10,
+    //     currentStatus: JourneyStatus.PENDING,
+    //     customer: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B45",
+    //     driver: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0B56",
+    //     ETA: 100,
+    //     jobId: "123",
+    //     journeyStart: 12,
+    //     journeyEnd: 13,
+    //     reciever: "0x9d4CCf6c3d6a1d5583c2918028c86Cc8267a0gr4",
+    //   },
+    // ]);
   }, []);
 
   return (
